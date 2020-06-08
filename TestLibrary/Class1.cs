2e88b6fb9c344604ed48f8d593ed9e7713efab79
Михаил Завёрткин <misha.zavertkin@mail.ru>
2020-06-08 15:21:29 +0700

    Adding test projects

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public static int X2(int arg)
+        {
+            return arg * 2;
+        }
+    }
+}
---
+++ TestProgramm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+namespace TestProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm;
+
+            using (BinaryReader reader = new BinaryReader(File.Open(@"C:\Users\Миша\source\repos\Project-calculis\TestLibrary\bin\Debug\netcoreapp3.1\TestLibrary.dll", FileMode.Open)))
+            {
+                asm = Assembly.Load(reader.ReadBytes((int)reader.BaseStream.Length));
+            }
+
+            
+
+            Type type = asm.GetType("TestLibrary.TestClass", true, true);
+
+            // создаем экземпляр класса Program
+            object obj = Activator.CreateInstance(type);
+
+            // получаем метод GetResult
+            MethodInfo method = type.GetMethod("X2");
+
+            // вызываем метод, передаем ему значения для параметров и получаем результат
+            object result = method.Invoke(obj, new object[] { 2 });
+            Console.WriteLine((result));
+            Console.Read();
+        }
+    }
+}
