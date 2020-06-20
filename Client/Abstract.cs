ab2f1876b29473ca39ac55f8bb349f2d79893e64
Misha <misha.zavertkin@mail.ru>
2020-06-20 22:52:01 +0700

    Adding abstract classes of Client

---
+++ Client/Abstract.cs
@@ -0,0 +1,70 @@
+namespace Client
+{
+    public abstract class Client
+    {
+
+        public readonly RemoteProxy RemoteProxy;
+
+        public readonly WorkManager WorkManager;
+
+    }
+
+    public abstract class RemoteProxy
+    {
+
+        public readonly Client Client;
+
+        public readonly Security Security;
+
+        public readonly ProjectEncoding ProjectEncoding;
+
+        public readonly Connection Connection;
+
+    }
+
+    public abstract class Security
+    {
+
+        public readonly Client Client;
+
+        public readonly RemoteProxy RemoteProxy;
+
+    }
+
+    public abstract class ProjectEncoding
+    {
+
+        public readonly Client Client;
+
+        public readonly RemoteProxy RemoteProxy;
+
+    }
+
+    public abstract class Connection
+    {
+
+        public readonly Client Client;
+
+        public readonly RemoteProxy RemoteProxy;
+
+        public bool IsOpen{get; private set;}
+
+        public byte[] Received {get; private set;}
+
+        public abstract bool Open(string ip, int port);
+
+        public abstract void Close();
+
+    }
+
+    public abstract class WorkManager
+    {
+
+        public readonly Client Client;
+
+        public abstract void SetCurrentWork(string path);
+
+        public abstract byte[] ExecuteCurrentWork(byte[] workSeed);
+
+    }
+}
--- ConcreteLibrary/CommonClasses.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AbstractClasses
-{
-    public class Class1
-    {
-    }
-}
